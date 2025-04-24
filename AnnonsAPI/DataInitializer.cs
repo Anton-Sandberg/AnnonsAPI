27b84df65a1471c0de133ce0bfd9257cfa31d807
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-24 18:34:45 +0200

    feat: add DataInitializer

---
+++ AnnonsAPI/DataInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AnnonsAPI
+{
+    public class DataInitializer
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DataInitializer(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Migrate()
+        {
+            _dbContext.Database.Migrate();
+        }
+    }
+}
