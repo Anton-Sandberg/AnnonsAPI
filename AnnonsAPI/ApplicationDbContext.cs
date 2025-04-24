a06fc44cfdb21577ae5d47c599a8adf1eb9d837a
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-24 18:34:31 +0200

    feat: add ApplicationDbContext

---
+++ AnnonsAPI/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AnnonsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnnonsAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Ad> Ads { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+    }
+}
